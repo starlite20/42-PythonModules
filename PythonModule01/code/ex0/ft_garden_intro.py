bd70cb9b058c8e1ac0c8f446b908016559170f29
Suhail <suhail542@gmail.com>
2026-03-05 01:44:46 +0400

    Working on Module02 - ex02

---
+++ PythonModule01/code/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+def ft_garden_intro():
+    print("=== Welcome to My Garden ===")
+    print("Plant: Rose")
+    print("Height: 25cm")
+    print("Age: 30 days")
+    print("\n=== End of Program ===")
+    
+
+if __name__ == "__main__":
+    ft_garden_intro()
---
+++ PythonModule01/code/ex2/ft_plant_growth.py
@@ -0,0 +1,41 @@
+class Plant:
+	def __init__(self, name: str, height: int, age_of_plant: int):
+		self.name = name
+		self.height = height
+		self.age_of_plant = age_of_plant
+	
+	def get_info(self):
+		return f"{self.name}: {self.height}cm, {self.age_of_plant} days old"
+
+	def grow(self):
+		self.height += 1
+
+	def age(self):
+		self.age_of_plant += 1
+
+if __name__=="__main__":
+	garden = [
+		Plant("Rose", 25, 30),
+		Plant("Sunflower", 80, 45),
+		Plant("Cactus", 15, 120)
+	]
+
+	initial_heights = []
+	for plant in garden:
+		initial_heights.append(plant.height)
+
+	print("=== Day 1 ===")
+	for plant in garden:
+		print(plant.get_info())
+
+	for day in range(6):
+		for plant in garden:
+			plant.grow()
+			plant.age()
+
+	print("=== Day 7 ===")
+	for plant in garden:
+		print(plant.get_info())
+
+	growth = garden[0].height - initial_heights[0]
+	print(f"Growth this week: +{growth}cm")
