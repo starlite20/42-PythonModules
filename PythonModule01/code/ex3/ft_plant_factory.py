34cc0e932ba329b74cb1d6303ec78567eea7516e
Suhail <suhail542@gmail.com>
2026-03-08 02:43:12 +0400

    Upto PMod1_ex4

---
+++ PythonModule01/code/ex3/ft_plant_factory.py
@@ -0,0 +1,27 @@
+class Plant:
+    def __init__(self, name: str, height: int, age_of_plant: int):
+        self.name = name
+        self.height = height
+        self.age_of_plant = age_of_plant
+
+    def creation_log(self):
+        print(
+            f"""Created: {self.name} """
+            f"""({self.height}cm, {self.age_of_plant} days)"""
+            )
+
+
+if __name__ == "__main__":
+    garden_data = [("Rose", 25, 30), ("Oak", 200, 365), ("Cactus", 5, 90),
+                   ("Sunflower", 80, 45), ("Fern", 15, 120)]
+    plants = []
+
+    print("=== Plant Factory Output ===")
+
+    for data in garden_data:
+        plants.append(Plant(*data))
+
+    for plant in plants:
+        plant.creation_log()
+
+    print(f"\nTotal plants created: {len(plants)}")
---
+++ PythonModule01/code/ex4/ft_garden_security.py
@@ -0,0 +1,48 @@
+class SecurePlant:
+    def __init__(self, name: str, height: int, age_of_plant: int) -> None:
+        self.name = name
+        print(f"Plant created: {self.name}")
+        self.set_height(height)
+        self.set_age(age_of_plant)
+
+    def set_height(self, height_passed) -> None:
+        if (height_passed < 0):
+            print(
+                f"""\nInvalid operation attempted: """
+                f"""height {height_passed}cm [REJECTED]"""
+                )
+            print("Security: Negative height rejected")
+        else:
+            self.height = height_passed
+            print(f"Height updated: {self.height}cm [OK]")
+
+    def set_age(self, age_passed) -> None:
+        if (age_passed < 0):
+            print(
+                f"\nInvalid operation attempted: """
+                f"""age {age_passed}days [REJECTED]"""
+                )
+            print("Security: Impossible age value rejected")
+        else:
+            self.age_of_plant = age_passed
+            print(f"Age updated: {self.age_of_plant} days [OK]")
+
+    def get_height(self) -> int:
+        return (self.height)
+
+    def get_age(self) -> int:
+        return (self.age_of_plant)
+        
+    def current_plant(self):
+        print(
+            f"""\nCurrent plant: {self.name} """
+            f"""({self.height}cm, {self.age_of_plant} days)"""
+            )
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+
+    plant = SecurePlant("Rose", 25, 30)
+    plant.set_height(-5)
+    plant.current_plant()
