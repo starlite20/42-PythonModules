ef1982f45130605e598ce64ff7d8202d5090d08b
Suhail <suhail542@gmail.com>
2026-04-05 02:19:00 +0400

    PM02-E1 completed

--- PythonModule02/code/ex0/ft_first_exception.py
+++ PythonModule02/code/ex0/ft_first_exception.py
@@ -3,7 +3,7 @@ def input_temperature(input_str: str) -> int:
     return int(input_str)
 
 
-def test_temperature(input_str: str) -> None:
+def call_input_temperature(input_str: str) -> None:
     try:
         temp = input_temperature(input_str)
         print(f"Temperature is now {temp}°C")
@@ -11,8 +11,12 @@ def test_temperature(input_str: str) -> None:
         print(f"Caught input_temperature error: {e}")
 
 
+def test_temperature() -> None:
+    call_input_temperature('25')
+    call_input_temperature('abc')
+    print("\nAll tests completed - program didn't crash!")
+
+
 if __name__ == "__main__":
     print("=== Garden Temperature ===")
-    test_temperature('25')
-    test_temperature('abc')
-    print("\nAll tests completed - program didn't crash!")
+    test_temperature()
---
+++ PythonModule02/code/ex1/ft_raise_exceptions.py
@@ -0,0 +1,30 @@
+def input_temperature(input_str: str) -> int:
+    print(f"\nInput data is '{input_str}'")
+    temp_val = int(input_str)
+    if (temp_val < 0):
+        raise ValueError(f"{temp_val}°C is too cold for plants (min 0°C)")
+    elif (temp_val > 40):
+        raise ValueError(f"{temp_val}°C is too hot for plants (max 40°C)")
+    else:
+        return temp_val
+
+
+def call_input_temperature(input_str: str) -> None:
+    try:
+        temp = input_temperature(input_str)
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+
+
+def test_temperature() -> None:
+    call_input_temperature('25')
+    call_input_temperature('abc')
+    call_input_temperature('100')
+    call_input_temperature('-50')
+    print("\nAll tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature Checker ===")
+    test_temperature()
