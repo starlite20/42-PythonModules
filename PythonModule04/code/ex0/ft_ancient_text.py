276ff21d29adbf2522d1695e31ea0145ac77fe5d
Suhail <suhail542@gmail.com>
2026-04-08 22:59:00 +0400

    Working on Module 04

---
+++ PythonModule04/code/ex0/ft_ancient_text.py
@@ -0,0 +1,33 @@
+import sys
+import typing
+
+def print_file_data(file: typing.IO[str]) -> None:
+    content = file.read()
+    print("---\n")
+    print(content)
+    print("\n---")
+    
+def main() -> None: 
+    if(len(sys.argv) != 2):
+        print("Usage: ft_ancient_text.py <file>\n")
+        return
+    
+    file = None
+    print("=== Cyber Archives Recovery ===")
+    try:
+        filename = sys.argv[1]
+        print(f"Accessing file '{filename}'")
+        file = open(filename, "r")
+        print_file_data(file)
+        
+    except Exception as e:
+        print(f"Error opening file '{filename}': {e}\n")
+        return
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File '{filename}' closed.")
+
+
+if __name__ == "__main__":
+    main()
